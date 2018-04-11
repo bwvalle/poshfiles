336bce244109bc790c314de4f93ac79ecc326db7
Bruno Valle <bwvalle@yahoo.com.br>
2018-04-11 16:19:34 -0300

    Addoh-my-posh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,13 +2,15 @@ $scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 # Modules
 Import-Module posh-git
-# Import-Module z
+Import-Module -Name oh-my-posh
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Set-Theme paradox
 
 # scripts
 gci "$scriptPath\scripts\*.ps1" | foreach { .$_ }
--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,9 @@ $scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 Install-Module -Name posh-git -AllowPrerelease -Force -Scope AllUsers
 Install-Module -Name Pester -Force -SkipPublisherCheck
+Install-Module -Name oh-my-posh -Scope CurrentUser
 
 # Install-Module z -Force -AllowClobber
 
-Write-Host "Restart Powershell!" -ForegroundColor Yellow
+Write-Output "Restart Powershell!"
 
