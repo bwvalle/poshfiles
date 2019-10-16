eca135fa58af1740fe1d6f1130e21e16cfd99888
Bruno Valle <bruno.valle@mstech.com.br>
2019-10-16 10:07:17 -0300

    deleting chocolatey settings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,11 +7,6 @@ $OutputEncoding = [Console]::OutputEncoding
 Import-Module posh-git
 Import-Module -Name oh-my-posh
 
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
 
 Set-Theme paradox
 
@@ -19,10 +14,6 @@ Set-Theme paradox
 $scripts = gci "$scriptPath\scripts\*.ps1"
 $scripts += (gci "$scriptPath\work\*.ps1" -ErrorAction SilentlyContinue)
 $scripts | foreach { .$_ }
-
-if (Test-Path("$scriptPath\work")) {
-  Import-Module "$ChocolateyProfile"
-}
 
 # Proxy
 [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
